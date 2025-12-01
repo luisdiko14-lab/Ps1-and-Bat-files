b559be802271e8a92f2b4bc906fa5361105738fe
luisdiko14-lab <luisdiko14@gmail.com>
2025-12-01 20:32:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("downloadBtn").addEventListener("click", function() {
     window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files/tree/main/Cool%20Folder%20With%20Files";
 });
+
+document.getElementById("viewRepo").addEventListener("click", function() {
+    window.location.href = "https://github.com/luisdiko14-lab/Ps1-and-Bat-files-and-more";
+});
